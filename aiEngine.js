df79a4fd383b90f70450ad15214a2bea84ac3b5b
muller king <mullerhihi@gmail.com>
2025-10-19 06:49:01 -0700

    deploy with dokploy

--- aiEngine.js
+++ aiEngine.js
@@ -6,7 +6,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 async function aiEngine(resource, count, attributes) {
   const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
 
-  const prompt = `Generate exactly ${count} dummy ${resource} objects as a valid JSON array. Each object must have the following fields: ${attributes.join(', ')}. Ensure values are realistic and varied. Output only the JSON array, no extra text or explanations.`;
+  const prompt = `Generate exactly ${count} dummy ${resource} objects as a valid JSON array. Each object must have the following fields: ${attributes.join(', ')}. Ensure values are realistic and varied. Output only the JSON array, no extra text or explanations and finally REMEMBER TO USET THE REAL AND WORKING IMAGES URLS USE THIS EXACTLY URL ENDPOINTS FOR IMAGES = https://placehold.co/400x300.png?text=use+your+own+image+here.`;
 
   const result = await model.generateContent(prompt);
   const text = result.response.text();
--- main.js
+++ main.js
@@ -11,11 +11,14 @@ const aiEngine = require('./aiEngine');
 app.get('/api/v1/generate', async (req, res) => {
   const { resource = 'user', count = '10', attributes = 'id,name,email,password,position,salary' } = req.query;
   const attrs = attributes.split(',');
+  console.log(req.query)
   const num = parseInt(count, 10);
 
   try {
+    console.log("Generating AI Response...");
     const aiResponse = await aiEngine(resource, num, attrs);
     res.json(aiResponse);
+    console.log("AI Response generated successfully.");
   } catch (error) {
     console.error('Error generating AI content:', error);
     res.status(500).send('Error generating AI content.');
